1c9721f9c29c1befefa3a0a567cac6f0b5d30e7d
tomaszmartin <tomasz.martin@gmail.com>
2021-02-01 20:11:22 +0100

    Fix missing imports in tests

--- tests/parsers/test_common.py
+++ tests/parsers/test_common.py
@@ -2,18 +2,18 @@ import datetime as dt
 
 import pytest
 
-from app.parsers import base
+from app.parsers import common
 
 
 def test_whitespace_trimming():
-    assert base.remove_whitespace("\n\nTE\n   \nST   ") == "TE ST"
+    assert common.remove_whitespace("\n\nTE\n   \nST   ") == "TE ST"
 
 
 def test_date_extraction():
     for date_str in ["2020-12-01", "Dec 1, 2020"]:
-        assert base.extract_date(date_str) == dt.date(2020, 12, 1)
+        assert common.extract_date(date_str) == dt.date(2020, 12, 1)
 
 
 def test_date_extraction_new_format():
     with pytest.raises(ValueError):
-        base.extract_date("01-12-2020")
+        common.extract_date("01-12-2020")
